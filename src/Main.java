97f84672a623773879e88da6e46069f5cbad5a45
Artem Aseev <artem.aseev@gmail.com>
2022-10-03 14:27:34 +0300

    Имплементация компаратора через лямбду в Main.java, deleted PersonSurnameLenghtComparator.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -15,11 +16,20 @@ public class Main {
         people.add(new Person("Annet", "Petrova Petrova", 29));
         people.add(new Person("Marina", "Petrova-Vasechkina-Gluxova", 50));
 
+        Comparator<Person> comparator = (o1, o2) -> {
+            String[] surnameWordsO1 = o1.getSurname().split("\\P{IsAlphabetic}+");
+            String[] surnameWordsO2 = o2.getSurname().split("\\P{IsAlphabetic}+");
+            if (surnameWordsO1.length < surnameWordsO2.length) {
+                return -1;
+            } else if (surnameWordsO1.length > surnameWordsO2.length) {
+                return 1;
+            } else {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+        };
 
-        Collections.sort(people, new PersonSurnameLenghtComparator());
+        Collections.sort(people, comparator);
         Collections.reverse(people);
         System.out.println(people);
-
-
     }
 }
